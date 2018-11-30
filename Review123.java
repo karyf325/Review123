55b67474aa73a747a773f4038699a10e7591a917
karyf325 <43179791+karyf325@users.noreply.github.com>
2018-11-30 12:43:55 -0500

    Add files via upload

---
+++ Review123.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; 
+
+public class Review123 {
+
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		
+		int x; 
+		int y; 
+		
+		x = 22; 
+		y = 24;
+		
+		boolean p= true;
+		boolean q= false;  
+		
+		if (x++ == 23)
+		{
+			System.out.print(p);
+		
+		}
+		else
+		{
+			System.out.print(q);
+		}
+		
+		
+		
+		
+		
+	}
+
+}
